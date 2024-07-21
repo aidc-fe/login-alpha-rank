797b082c33bc7f26342ca4b1e9e9780adbedbea3
容悦 <xieningyu.xny@alibaba-inc.com>
2024-07-22 01:26:27 +0800

    chore: 解决构建问题

--- app/shopify/auth/page.tsx
+++ app/shopify/auth/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
+import SuspenseWrapper from "@/components/suspend-wrapper";
 import { getCsrfToken } from "next-auth/react";
 import { useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 
 const redirectUri = `${process.env.NEXT_PUBLIC_NEXT_AUTH_URL}/api/shopify/auth/callback`;
 
-export default function ShopifyAuthPage() {
+function ShopifyAuthPage() {
   const searchParams = useSearchParams();
   const shopDomain = searchParams.get("shopDomain");
 
@@ -15,8 +16,17 @@ export default function ShopifyAuthPage() {
       return;
     }
     getCsrfToken().then((csrfToken) => {
-      // setCsrfToken(res!);
       window.location.href = `https://${shopDomain}/admin/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_SHOPIFY_CLIENT_ID}&scope=read_products,write_products&redirect_uri=${redirectUri}&state=${csrfToken}`;
     });
   }, [shopDomain]);
+
+  return <></>;
+}
+
+export default function PageWrapper() {
+  return (
+    <SuspenseWrapper>
+      <ShopifyAuthPage />
+    </SuspenseWrapper>
+  );
 }
