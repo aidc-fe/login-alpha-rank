b10e8016b841c920f1a6a000806ecc11a188cfdd
容悦 <xieningyu.xny@alibaba-inc.com>
2024-07-22 01:25:00 +0800

    chore: 解决构建问题

--- app/shopify/login/page.tsx
+++ app/shopify/login/page.tsx
@@ -1,20 +1,34 @@
 "use client";
 
+import SuspenseWrapper from "@/components/suspend-wrapper";
 import { signIn } from "next-auth/react";
 import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
 const callbackUrl = `${process.env.NEXT_PUBLIC_NEXT_AUTH_URL}/login-landing-page`;
 
-export default function ShopifyAuthLoginPage() {
+function ShopifyAuthLoginPage() {
   const searchParams = useSearchParams();
 
-  const userData = Array.from(searchParams.keys()).reduce(
-    (acc: { [key: string]: string }, curr: string) => {
-      acc[curr] = searchParams.get(curr) || "";
-      return acc;
-    },
-    {}
-  );
+  useEffect(() => {
+    const userData = Array.from(searchParams.keys()).reduce(
+      (acc: { [key: string]: string }, curr: string) => {
+        acc[curr] = searchParams.get(curr) || "";
+        return acc;
+      },
+      {}
+    );
 
-  signIn("shopify", { ...userData, callbackUrl });
+    signIn("shopify", { ...userData, callbackUrl });
+  }, [searchParams]);
+
+  return <div>Redirecting...</div>;
+}
+
+export default function PageWrapper() {
+  return (
+    <SuspenseWrapper>
+      <ShopifyAuthLoginPage />
+    </SuspenseWrapper>
+  );
 }
