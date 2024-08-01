ae10c71efd340c00b28be26b00afebb87345c7f1
容悦 <xieningyu.xny@alibaba-inc.com>
2024-08-01 17:16:36 +0800

    feat: 添加数据库初始化命令

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
-
 import SessionProvider from "@/providers/session-provider";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme/provider";
