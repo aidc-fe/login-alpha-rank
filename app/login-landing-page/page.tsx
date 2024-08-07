f3da96662b588e5f0e5d3dcb19f1c798314170e8
yuyuqueen <578096684@qq.com>
2024-08-07 11:01:37 +0800

    style: 去除多余代码

--- app/login-landing-page/page.tsx
+++ app/login-landing-page/page.tsx
@@ -29,13 +29,9 @@ function PageContent() {
     switch (status) {
       case "authenticated":
         if (jwtToken) {
-          thirdPartySignIn(jwtToken, shopDomain)
-            .then(() => {
-              window.location.href = targetUrl;
-            })
-            .catch(() => {
-              window.location.href = targetUrl;
-            });
+          thirdPartySignIn(jwtToken, shopDomain).then(() => {
+            window.location.href = targetUrl;
+          });
         } else {
           router.replace(`/${searchParams.toString()}`);
         }
