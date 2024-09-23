3ebe4c5bd94529b2ee8c41d84e08d831bde4b9ac
yuyuqueen <578096684@qq.com>
2024-09-23 18:57:38 +0800

    feat: 完善signup流程

--- app/api/signUp/email/verify/route.ts
+++ app/api/signUp/email/verify/route.ts
@@ -1,3 +1,4 @@
+import { setSessionTokenCookie } from "@/lib/auth";
 import { createUser, validateMagicLink } from "@/lib/database";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -6,12 +7,24 @@ export async function GET(request: NextRequest) {
 
   try {
     const info = await validateMagicLink(token || "");
-    await createUser({
+    const userInfo = {
       password: info.password!,
       email: info.identifier,
       name: info.name!,
-    });
-    return NextResponse.redirect(process.env.NEXT_AUTH_URL!, { status: 302 });
+    };
+    await createUser(userInfo);
+
+    console.log(info);
+    const response = NextResponse.redirect(
+      `${process.env.NEXT_AUTH_URL}/login-landing-page?targetUrl=${info.targetUrl}`,
+      {
+        status: 302,
+      }
+    );
+
+    await setSessionTokenCookie(userInfo, response);
+
+    return response;
   } catch {
     return NextResponse.redirect(process.env.NEXT_AUTH_URL!, { status: 302 });
   }
