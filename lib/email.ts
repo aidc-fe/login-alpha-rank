46a5ae590ec8bee962facdff520138f9562dc03d
盛杰文 <sjw01869413@alibaba-inc.com>
2024-10-18 14:04:43 +0800

    feat: style

--- lib/email.ts
+++ lib/email.ts
@@ -26,52 +26,18 @@ export async function sendVerificationEmail(
     to, // 收件人
     subject,
     html: `
-      <style>
-        .copy-link {
-            cursor: pointer;
-            font-size: 16px;
-            line-height: 24px;
-            color: #7c3aed;
-            word-break: break-all;
-        }
-      </style>
       <h1 style="color: rgb(32, 33, 35); font-size: 32px; line-height: 40px; margin: 0px 0px 20px;"> ${displayContent.title}</h1>
       <p style="font-size: 16px; line-height: 24px;"> ${displayContent.description}</p>
-      <table style="border-collapse: collapse; width: 100%; border: none; margin-top: 24px;">
+      <table style="border-collapse: collapse; width: 100%; border-spacing: 20px; border: none; margin-top: 24px;">
         <tr>
-            <td style="vertical-align: top; border: none; padding: 0; margin: 0; width: 100px;">
-                <a href=${verificationLink} style="display: inline-block; text-decoration: none; background: #7c3aed; border-radius: 6px; color: white; font-size: 16px; line-height: 48px; text-align: center; font-weight: 500; width: 80px; height: 48px; margin: 0px;" target="_blank">${displayContent.btnContent}</a>
-            </td>
-            <td style="vertical-align: top; border: none; padding: 0; margin: 0; text-align: left;">
-                <span class="copy-link">copy link</span>
-            </td>
+          <td style="vertical-align: top; border: none; padding: 0; margin: 0; width: 100px;">
+              <a href=${verificationLink} style="display: inline-block; text-decoration: none; background: #7c3aed; border-radius: 6px; color: white; font-size: 16px; line-height: 48px; text-align: center; font-weight: 500; width: 80px; height: 48px; margin: 0px;" target="_blank">${displayContent.btnContent}</a>
+          </td>
+          <td style="vertical-align: top; border: none; padding: 0; margin: 0;">
+            <div style="padding-left: 16px;"><a href=${verificationLink} style="font-size: 14px; line-height: 20px; color: #7c3aed; word-break: break-all;" target="_blank">${verificationLink}</a></div>
+          </td>
         </tr>
       </table>
-    <script>
-      window.onload = function() {
-            document.querySelector('.copy-link').addEventListener('click', function() {
-                copyLink();
-            });
-        }
-
-        function copyLink() {
-            const verificationLink = '${verificationLink}';
-            const tempInput = document.createElement('input');
-            
-            // 设置 input 样式
-            tempInput.style.position = 'absolute';
-            tempInput.style.left = '-9999px';
-            tempInput.style.top = '-9999px';
-            tempInput.style.opacity = '0';
-            
-            tempInput.value = verificationLink;
-            document.body.appendChild(tempInput);
-            tempInput.select();
-            document.execCommand('copy');
-            document.body.removeChild(tempInput);
-            alert('链接已复制到剪贴板');
-        }
-    </script>
     `,
   };
 
