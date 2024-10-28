0657c0b1abd16d693ff785db73c0420913badf23
yuyuqueen <578096684@qq.com>
2024-10-28 15:10:47 +0800

    fix: 正常情况下会关闭页面

--- app/(2C)/login-landing-page/page.tsx
+++ app/(2C)/login-landing-page/page.tsx
@@ -28,7 +28,7 @@ function PageContent() {
         if (jwtToken) {
           thirdPartySignIn(jwtToken, shopDomain).then(() => {
             // 如果是在登录弹窗中，则关闭弹窗
-            if (window.opener && window.opener !== window.self) {
+            if (window.opener && searchParams.get("isLoginPopup")) {
               window.close();
               return;
             }
