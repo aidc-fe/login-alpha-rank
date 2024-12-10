5c69ad3aadef5b3e1e327a34cd2ab64975c75d62
yuyuqueen <578096684@qq.com>
2024-12-10 19:47:11 +0800

    fix: xianshangwenti

--- app/api/shoplazza/callback/route.ts
+++ app/api/shoplazza/callback/route.ts
@@ -69,8 +69,6 @@ async function getShopInfo(
 }
 
 export async function GET(request: NextRequest) {
-  // const user = await createOrUpdateUser({ email:'123', from: "shoplazza", businessDomainId: '13883979-d8ac-41db-a930-2a152f8b3c90' });
-  // console.log({user})
   try {
     const code = request.nextUrl.searchParams.get("code");
     const state = request.nextUrl.searchParams.get("state") || "";
@@ -127,9 +125,8 @@ export async function GET(request: NextRequest) {
       user_name: shopInfo.name,
     };
 
-    console.log({userInfo,accountInfo})
     // 创建或更新用户信息和oAuth账号信息
-    const user = await createOrUpdateUser({ ...userInfo, from: "shoplazza", businessDomainId: '13883979-d8ac-41db-a930-2a152f8b3c90	' });
+    const user = await createOrUpdateUser({ ...userInfo, from: "shoplazza", businessDomainId: '13883979-d8ac-41db-a930-2a152f8b3c90' });
     await createOrUpdateAccount({
       ...accountInfo,
       userId: user.id,
